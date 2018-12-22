608228233db49fad5a80c4c1dcd6bf232669599e
DawidSzczesniakLM <dawidszczesniakluxmed@gmail.com>
2018-12-22 20:49:54 +0100

    Initial commit

---
+++ app/src/main/java/com/dawidszczesniak/test2020/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dawidszczesniak.test2020;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
